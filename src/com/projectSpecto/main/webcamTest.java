a02e188be2a928da56726bcced06ec3ce5a82e5a
J1tt3r <J1tt3r@users.noreply.github.com>
2015-08-20 22:05:33 +0200

    Test ob Webcam vorhanden ist.

--- src/com/projectSpecto/main/webcamTest.java
+++ src/com/projectSpecto/main/webcamTest.java
@@ -1,11 +1,25 @@
 package com.projectSpecto.main;
 
-import javax.swing.JFrame;
+// import javax.swing.JFrame;
 
 import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
+//import com.github.sarxos.webcam.WebcamPanel;
+//import com.github.sarxos.webcam.WebcamResolution;
 
+
+public class webcamTest {
+
+	public static void main(String[] args) {
+		Webcam webcam = Webcam.getDefault();
+		if (webcam != null) {
+			System.out.println("Webcam: " + webcam.getName());
+		} else {
+			System.out.println("No webcam detected");
+		}
+	}
+}
+
+/*
 
 public class webcamTest {
 
@@ -27,4 +41,4 @@ public class webcamTest {
 		window.pack();
 		window.setVisible(true);
 	}
-}
+} */
